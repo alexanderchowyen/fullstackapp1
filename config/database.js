bc86d2b04746c45df2021ae003df95a1f68367c9
Alexander Chow-Yen <89674474+alexanderchowyen@users.noreply.github.com>
2021-10-26 22:29:51 -0400

    Add files via upload

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb+srv://devchow:devchow@cluster0.julzg.mongodb.net/produce?retryWrites=true&w=majority', 
+    'dbName': 'produce'
+};
